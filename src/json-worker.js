31d5086fdae76353863df634195542f0e3c0763f
Leonardo Alfaya Fonseca <leo.alfaya@gmail.com>
2018-03-21 23:58:53 -0300

    creation file tested

--- src/json-worker.js
+++ src/json-worker.js
@@ -1,7 +1,17 @@
-module.exports = {
-    createFile: function() {},
-    readFile  : function() {},
-    saveData: function() {},
-    updateData: function() {},
-    deleteData: function() {},
+import fs from 'fs';
+import path from 'path';
+
+const createFile = (dirname, name) => {
+  fs.writeFileSync(path.join(dirname, name), JSON.stringify({teste:"teste salvando arquivo"}));
 }
+
+const deleteFile = (dirname, name) => {
+  fs.unlinkSync(path.join(dirname, name));
+}
+
+const readFile = () => {}
+const saveData = () => {}
+const updateData = () => {}
+const deleteData = () => {}
+
+export {createFile, deleteFile, readFile, saveData, updateData, deleteData}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,41 +1,64 @@
-const expect = require('chai').expect;
-const json_worker = require('../src/json-worker.js');
+import * as chai from 'chai';
+import { createFile, deleteFile, readFile, saveData, updateData, deleteData } from '../src/json-worker.js';
+import * as path from 'path';
+
+const expect = chai.expect;
+
+chai.use(require('chai-fs'));
 
 describe('JSON Worker', function(){
   describe('smoke tests', function(){
-    it('should exist the json-worker lib', function(){
-      expect(json_worker).to.exist;
+    it('should exist jsonWorker createFile method', function(){
+      expect(createFile).to.exist;
+      expect(createFile).to.be.a("function");
     });
 
-    it('should exist json_worker.createFile method', function(){
-      expect(json_worker.createFile).to.exist;
-      expect(json_worker.createFile).to.be.a("function");
-    })
-
-    it('should exist json_worker.readFile method', function(){
-      expect(json_worker.readFile).to.exist;
-      expect(json_worker.readFile).to.be.a("function");
-    })
-
-    it('should exist json_worker.saveData method', function(){
-      expect(json_worker.saveData).to.exist;
-      expect(json_worker.saveData).to.be.a("function");
-    })
-
-    it('should exist json_worker.updateData method', function(){
-      expect(json_worker.updateData).to.exist;
-      expect(json_worker.updateData).to.be.a("function");
-    })
-
-    it('should exist json_worker.deleteData method', function(){
-      expect(json_worker.deleteData).to.exist;
-      expect(json_worker.deleteData).to.be.a("function");
-    })
-
-  })
-  describe('Read or create JSON file', function(){
-    it('should create JSON file called album.json', function(){
-      
+    it('should exist jsonWorker createFile method', function(){
+      expect(deleteFile).to.exist;
+      expect(deleteFile).to.be.a("function");
     });
+
+    it('should exist jsonWorker readFile method', function(){
+      expect(readFile).to.exist;
+      expect(readFile).to.be.a("function");
+    });
+
+    it('should exist jsonWorker saveData method', function(){
+      expect(saveData).to.exist;
+      expect(saveData).to.be.a("function");
+    });
+
+    it('should exist jsonWorker updateData method', function(){
+      expect(updateData).to.exist;
+      expect(updateData).to.be.a("function");
+    });
+
+    it('should exist jsonWorker deleteData method', function(){
+      expect(deleteData).to.exist;
+      expect(deleteData).to.be.a("function");
+    });
+  });
+
+  describe('Create JSON file', function(){
+    let dirname;
+    let fileName;
+
+    before(function(){
+      dirname = path.normalize(`${path.dirname(__dirname)}/data`);
+      fileName = "teste.json";
+    });
+
+    it('should not exist the teste.json file', function(){
+      expect(dirname).to.be.a.directory().and.not.have.contents([fileName]);
+    });
+
+    it('should create the teste.json file', function(){
+      createFile(dirname, fileName);
+      expect(dirname).to.be.a.directory().with.contents([fileName]);
+    });
+
+    after(function(){
+      deleteFile(dirname, fileName);
+    })
   });
 });
